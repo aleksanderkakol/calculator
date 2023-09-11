b4fc1506ed82b76216e7d651ab0b434e244f528b
Aleksander Kąkol <olcio45@gmail.com>
2023-09-11 15:09:29 +0200

    Added twig bundle, cli command and unit tests

---
+++ src/Controller/CalculatorController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CalculatorController extends AbstractController
+{
+    #[Route(path: '/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('calculator/calculator.html.twig');
+    }
+}
--- src/Service/Calculator/CalculatorInterface.php
+++ src/Service/Calculator/CalculatorInterface.php
@@ -4,5 +4,7 @@ namespace App\Service\Calculator;
 
 interface CalculatorInterface
 {
+    public function canBeUsed(string $operation): bool;
+
     public function calculate(float $first, float $second): float;
 }
